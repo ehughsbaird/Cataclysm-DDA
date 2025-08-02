82a40088c600bdd233d263c7f04aa3da5fc2914a
ehughsbaird <44244083+ehughsbaird@users.noreply.github.com>
2025-08-02 00:34:14 +0000

    trap optional/mandatory

--- src/trap.h
+++ src/trap.h
@@ -82,6 +82,8 @@ struct vehicle_handle_trap_data {
     // the double represents the count or chance to spawn.
     std::vector<std::pair<itype_id, double>> spawn_items;
     trap_str_id set_trap = trap_str_id::NULL_ID();
+
+    void deserialize( const JsonObject &jo );
 };
 
 using trap_function = std::function<bool( const tripoint_bub_ms &, Creature *, item * )>;
@@ -157,8 +159,15 @@ struct trap {
          */
         std::pair<int, int> sound_threshold = {0, 0};
         int funnel_radius_mm = 0;
+
+        struct comp {
+            itype_id item_type;
+            int quantity;
+            int charges;
+            void deserialize( const JsonValue &jv );
+        };
         // For disassembly?
-        std::vector<std::tuple<itype_id, int, int>> components;
+        std::vector<comp> components;
     public:
         std::optional<itype_id> trap_item_type;
         // data required for trapfunc::spell()
